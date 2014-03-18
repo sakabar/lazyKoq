252a8f7e6771edb9aaec129524af00a4fa73e1b0
sakaki <takafumi.sakakibara.pdx@gmail.com>
2014-03-18 12:15:56 +0900

    add Hypothesis

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ lazyKoq
 =======
 
 interpreter of lazy K written by Coq
+
+TODO:
+1. function of (lambda formula TO combinator)
+2. function of (string TO Church number)
+3. function of (Church number TO string)
--- lazyKoq.v
+++ lazyKoq.v
@@ -1,3 +1,5 @@
+Require Import Recdef.
+
 Module LazyKoq.
 (* Variable Input : Expr. *)
 
@@ -17,58 +19,35 @@ Inductive evalR : Expr -> Expr -> Prop :=
     evalR (app I x) xr
     
   | eval_K0 : evalR K K
-  | eval_K1 : forall (x:Expr),
-    evalR (app K x) (app K x)
+  | eval_K1 : forall (x xr:Expr),
+    evalR x xr ->
+    evalR (app K x) (app K xr)
   | eval_K2 : forall (x y xr :Expr),
     evalR x xr ->
     evalR (app (app K x) y) xr
 
   | eval_S0 : evalR S S
-  | eval_S1 : forall (x:Expr),
-    evalR (app S x) (app S x)
-  | eval_S2 : forall (x y:Expr),
-    evalR (app (app S x) y) (app (app S x) y)
+  | eval_S1 : forall (x xr:Expr),
+    evalR x xr ->
+    evalR (app S x) (app S xr)
+  | eval_S2 : forall (x y xr yr:Expr),
+    evalR x xr ->
+    evalR y yr ->
+    evalR (app (app S x) y) (app (app S xr) yr)
   | eval_S3 : forall (x y z xzr yzr res: Expr),
     evalR (app x z) xzr ->
     evalR (app y z) yzr ->
     evalR (app xzr yzr) res ->
     evalR (app (app (app S x) y) z) res
-
-  (* | rm_eval_I : forall (x res:Expr), *)
-  (*   evalR (app I x) res -> *)
-  (*   evalR x res *)
-
-  (* | rm_eval_K : forall (x y res:Expr), *)
-  (*   evalR (app (app K x) y) res -> *)
-  (*   evalR x res *)
-
-  (* | rm_eval_S : forall (x y z res:Expr), *)
-  (*   evalR (app (app (app S x) y) z) res -> *)
-  (*   evalR (app (app x z) (app y z)) res *)
 .
 
 (* 前置演左子は使えない? *)
 (* Locate "`". *)
 (* Notation "'`' x y" := (app x y) (at level 40). *)
 
-Example testI: evalR (app I I) I.
-Proof.
-  apply eval_I1.
-  apply eval_I0.
-Qed.
-
-Theorem SKK: forall (e er:Expr), evalR e er -> evalR (app (app (app S K) K) e) er.
-Proof.
-  intros e er H0.
-  apply eval_S3 with (xzr := (app K e)) (yzr := (app K e)).
-  apply eval_K1.
-  apply eval_K1.
-  apply eval_K2.
-  assumption.
-Qed.
-
-Definition zero := (app K I).
-Definition one := (app (app S (app (app S (app K S)) (app (app S (app K K)) I))) I).
+Hypothesis I_eq: forall x:Expr, app I x = x.
+Hypothesis K_eq: forall x y:Expr, (app (app K x) y) = x.
+Hypothesis S_eq: forall x y z:Expr, (app (app (app S x) y) z) = (app (app x z) (app y z)).
 
 (* inner_evalを証明するために要請された *)
 Lemma eval_I_res: forall(r:Expr), evalR I r <-> r = I.
@@ -81,12 +60,62 @@ Qed.
 (* eval_I_res と類似の補題 *)
 Lemma eval_K_res: forall(r:Expr), evalR K r <-> r = K.
 Proof.
-  Admitted.
+  intros r.
+  split; intros H0; destruct r; try reflexivity; try inversion H0.
+  apply eval_K0.
+Qed.
 
 (* eval_I_res と類似の補題 *)
 Lemma eval_S_res: forall(r:Expr), evalR S r <-> r = S.
 Proof.
-  Admitted.
+  intros r.
+  split; intros H0; destruct r; try reflexivity; try inversion H0.
+  apply eval_S0.
+Qed.
+
+Example testI: evalR (app I I) I.
+Proof.
+  apply eval_I1.
+  apply eval_I0.
+Qed.
+
+(* SKKを証明するために要請された *)
+(* 評価の結果はそれ以上簡約されない *)
+(* この定理は成り立たない。反例は SII(SII) => SII(SII) *)
+Theorem value_is_reduced: forall(e er:Expr), evalR e er -> evalR er er.
+Proof.
+Abort.
+
+Theorem SKK: forall (e er:Expr), evalR e er -> evalR (app (app (app S K) K) e) er.
+Proof.
+  intros e er H0.
+  rewrite S_eq.
+  rewrite K_eq.
+  assumption.
+Qed.
+
+Definition zero := (app K I).
+Definition one := I.
+Definition two := (app (app S (app (app S (app K S)) K)) I).
+Definition succ := (app S (app (app S (app K S)) K)).
+
+Theorem succ_zero_is_one: forall f n:Expr,
+  (app (app (app succ zero) f) n) = (app (app one f) n).
+Proof.
+  unfold succ.
+  unfold zero.
+  unfold one.
+  intros.
+  rewrite I_eq.
+  rewrite S_eq with (x := (app (app S (app K S)) K)) (y := (app K I)) (z := f).
+  rewrite K_eq with (x := I) (y := f).
+  rewrite S_eq with (x := (app K S)).
+  rewrite K_eq.
+  rewrite S_eq.
+  rewrite I_eq.
+  rewrite K_eq.
+  reflexivity.
+Qed.
 
 Lemma eval_appI_res: forall(e r:Expr), evalR e r -> evalR (app I e) r.
 Proof.
@@ -97,41 +126,36 @@ Qed.
 
 Lemma eval_appI_res_inv: forall(e r:Expr), evalR (app I e) r -> evalR e r.
 Proof.
-  intros e r H0.
-  induction e.
-  apply eval_I_res.
-  assert (evalR (app I I) I).
-  apply eval_I1.
-  apply eval_I0.
-Abort.
+  intros.
+  rewrite I_eq in H.
+  assumption.
+Qed.
 
 (* inner_evalを証明するために要請された *)
 (* 評価の結果は1通りである *)
-Lemma eval_eq: forall(e1 e2 r1 r2:Expr),
-  evalR e1 r1 -> evalR e2 r2 -> e1 = e2 -> r1 = r2.
+(* これも、偽の命題である。反例は SII(SII)*)
+Lemma eval_eq: forall(e r1 r2:Expr),
+  evalR e r1 -> evalR e r2 -> r1 = r2.
 Proof.
-  intros e1 e2 r1 r2 H0 H1 H2.
-  rewrite <- H2 in H1.
-  destruct e1.
-  apply eval_I_res in H0.
-  apply eval_I_res in H1.
-  rewrite H0.
-  rewrite H1.
-  reflexivity.
-
-  apply eval_K_res in H0.
-  apply eval_K_res in H1.
-  rewrite H0.
-  rewrite H1.
-  reflexivity.
-
-  apply eval_S_res in H0.
-  apply eval_S_res in H1.
-  rewrite H0.
-  rewrite H1.
-  reflexivity.
-
 Abort.
+
+(* 簡約、関数による表現 *)
+(* 停止性を示せない *)
+(* Function reduce(e:Expr) {measure app_length e}: Expr := *)
+(*   match e with *)
+(*   | app I x => reduce x *)
+(*   | I => I *)
+
+(*   | (app (app K x) y) => (reduce x) *)
+(*   | K => K *)
+(*   | app K x => app K (reduce x) *)
+
+(*   | (app (app (app S x) y) z) => (app (app (reduce x) (reduce z)) (app (reduce y) (reduce z))) *)
+(*   | (app (app S x) y) => (app (app S (reduce x)) (reduce y)) *)
+(*   | S => S *)
+
+(*   | app e1 e2 => (app (reduce e1) (reduce e2)) *)
+(*   end. *)
 
 (* testZeroを証明するために要請された *)
 Theorem inner_eval: forall (e1 e2 r1 res:Expr),
@@ -149,19 +173,6 @@ Proof.
   assert (evalR (app I I) I).
   apply eval_I1.
   apply eval_I0.
-Abort.
-
-Example testZero : forall (s z res : Expr), evalR z res -> evalR (app (app zero s) z) res.
-Proof.
-  intros s z res H0.
-  unfold zero.
-  Abort.
-
-
-Example testOne : forall(s z res:Expr), evalR (app s z) res -> evalR (app (app one s) z) res.
-Proof.
-  intros s z res H0.
-  unfold one.
 Abort.
 
 End LazyKoq.
